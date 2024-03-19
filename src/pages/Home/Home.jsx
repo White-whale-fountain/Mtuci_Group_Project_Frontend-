c0fca183e5eab7b559ce02181eff5fe474086bad
Quzzqq <black65reg@gmail.com>
2024-03-19 16:53:47 +0300

    Проверка axios

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,23 @@
 import "./Home.module.css";
 import Home_Header from "./Home_Header/Home_Header";
 import Screen from "./Screen/Screen";
+import axios from "axios";
 
 export default function Home() {
+  const instance = axios.create({
+    baseURL: "http://127.0.0.1:5000",
+  });
+
+  function getData() {
+    instance
+      .post("/login", { email: "WINNER@gmail.com", password: "525252" })
+      .then((response) => {
+        console.log(response.data.access_token);
+      });
+  }
   return (
     <>
+      <button onClick={getData}>123</button>
       <Home_Header />
       <Screen />
     </>
