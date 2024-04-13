11904881d851553e01d147b150b53b35272e54c9
Quzzqq <black65reg@gmail.com>
2024-04-13 13:38:29 +0300

    Незначительные изменения профиля
    -корректировка css-а на страничке профиля

--- src/pages/Profile/ProfileHeader/ProfileHeader.jsx
+++ src/pages/Profile/ProfileHeader/ProfileHeader.jsx
@@ -1,38 +1,68 @@
-import styles from './ProfileHeader.module.css'
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import styles from "./ProfileHeader.module.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function ProfileHeader() {
-    const [isOpen, setIsOpen] = useState()
-    function leave() {
-        return localStorage.removeItem("user")
-    }
+  const [isOpen, setIsOpen] = useState();
+  function leave() {
+    return localStorage.removeItem("user");
+  }
 
-    return (
-        <div className={styles.profile_header}>
-            <img src='/../../src/assets/img/profile.png' alt='profile' className={styles.profile_header_profile}/>
-            <img src='/../../src/assets/img/bell.png' alt='Уведомления' className={styles.profile_header_notifications}/>
-            <nav className={styles.profile_menu}>
-                <button className={styles.profile_header_dropdown} onClick={() => setIsOpen(!isOpen)}>
-                    <img src='/../../src/assets/img/people1.png' alt='logo' width={"45px"} height={"45px"} className={styles.profile_user_img}/>
-                    <img src='/../../src/assets/img/profile_vector_dropdown.png' alt='' className={styles.dropdown_button_vector}/>
-                </button>
-                {isOpen &&
-                <ul className={styles.profile_menu_list}>
-                    <li key={0} className={styles.profile_menu_listitem}>Мои лайки</li>
-                    <hr/>
-                    <li key={1} className={styles.profile_menu_listitem}>Друзья</li>
-                    <hr/>
-                    <li key={2} className={styles.profile_menu_listitem}>
-                        <Link to={"/"}>
-                          <button onClick={leave} className={styles.profile_menu_leave_button}>
-                            Выйти из аккаунта
-                          </button>
-                        </Link>
-                    </li>
-                </ul>}
-            </nav>
-
-        </div>
-    )
+  return (
+    <div className={styles.profile_header}>
+      <img
+        src="/../../src/assets/img/profile.png"
+        alt="profile"
+        className={styles.profile_header_profile}
+      />
+      <div className={styles.profile_right}>
+        <img
+          src="/../../src/assets/img/bell.png"
+          alt="Уведомления"
+          className={styles.profile_header_notifications}
+        />
+        <nav className={styles.profile_menu}>
+          <button
+            className={styles.profile_header_dropdown}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <img
+              src="/../../src/assets/img/people1.png"
+              alt="logo"
+              width={"45px"}
+              height={"45px"}
+              className={styles.profile_user_img}
+            />
+            <img
+              src="/../../src/assets/img/profile_vector_dropdown.png"
+              alt=""
+              className={styles.dropdown_button_vector}
+            />
+          </button>
+          {isOpen && (
+            <ul className={styles.profile_menu_list}>
+              <li key={0} className={styles.profile_menu_listitem}>
+                Мои лайки
+              </li>
+              <hr />
+              <li key={1} className={styles.profile_menu_listitem}>
+                Друзья
+              </li>
+              <hr />
+              <li key={2} className={styles.profile_menu_listitem}>
+                <Link to={"/"}>
+                  <button
+                    onClick={leave}
+                    className={styles.profile_menu_leave_button}
+                  >
+                    Выйти из аккаунта
+                  </button>
+                </Link>
+              </li>
+            </ul>
+          )}
+        </nav>
+      </div>
+    </div>
+  );
 }
--- src/pages/Profile/ProfileMainBlock/ProfileMainBlock.jsx
+++ src/pages/Profile/ProfileMainBlock/ProfileMainBlock.jsx
@@ -1,10 +1,10 @@
-import styles from "./ProfileMainBlock.module.css"
-import { profile } from '../../../service/profile.js'
-import { useState, useEffect } from "react"
-import ProfileCard from './components/ProfileCard/ProfileCard.jsx'
-import ProfileDescription from './components/ProfileDescription/ProfileDescription.jsx'
+import styles from "./ProfileMainBlock.module.css";
+import { profile } from "../../../service/profile.js";
+import { useState, useEffect } from "react";
+import ProfileCard from "./components/ProfileCard/ProfileCard.jsx";
+import ProfileDescription from "./components/ProfileDescription/ProfileDescription.jsx";
 
-export default function ProfileMainBlock(){
+export default function ProfileMainBlock() {
   const login = JSON.parse(localStorage.getItem("user"));
   const [data, setData] = useState([]);
 
@@ -16,22 +16,21 @@ export default function ProfileMainBlock(){
     fetchData();
   }, []);
 
-
-    return(
-        <div className={styles.profile_main_block}>
-            <img src='/../../src/assets/img/profile_background.png' alt='' className={styles.profile_main_background} />
-            <ProfileCard>
-              {
-                {
-                  'name': data.name,
-                  'age': data.age,
-                  'sex': data.sex
-                }
-              }
-            </ProfileCard>
-            <ProfileDescription/>
-
-
-        </div>
-    )
+  return (
+    <div className={styles.profile_main_block}>
+      <img
+        src="/../../src/assets/img/profile_background.png"
+        alt=""
+        className={styles.profile_main_background}
+      />
+      <ProfileCard>
+        {{
+          name: data.name,
+          age: data.age,
+          sex: data.sex,
+        }}
+      </ProfileCard>
+      <ProfileDescription />
+    </div>
+  );
 }
